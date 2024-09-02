1678fdaf673feb6e921b66adb7524b6bbfc9e2cc
Igor Nascimento <igor.nacimento51@gmail.com>
2024-09-02 12:54:20 -0300

    feat: add HelloController to swagger

--- src/main/java/com/abreu/authtest/controller/HelloController.java
+++ src/main/java/com/abreu/authtest/controller/HelloController.java
@@ -1,5 +1,10 @@
 package com.abreu.authtest.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +12,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/v1/hello")
+@Tag(name = "Hello", description = "Endpoint for testing authentication")
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping
+    @Operation(summary = "Say hello to user!",
+        description = "Say hello to user!",
+        tags = {"Hello"},
+        responses = {
+            @ApiResponse(description = "Success", responseCode = "200",
+                content = @Content(schema = @Schema(implementation = String.class))),
+            @ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),
+        })
     public String hello() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         System.out.println(authentication);
